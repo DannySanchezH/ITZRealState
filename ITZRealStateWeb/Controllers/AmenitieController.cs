9af8fdf81d12f6b11fe64e9bd7134f4144c1f303
Daniela Sanchez <dannysarahy@hotmail.com>
2013-04-17 09:27:23 -0500

    Web app introduction
    Confirmar funcionalidad de la api

---
+++ ITZRealStateWeb/Controllers/AmenitieController.cs
@@ -0,0 +1,110 @@
+﻿using ITZRealStateWeb.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITZRealStateWeb.Models;
+
+namespace ITZRealStateWeb.Controllers
+{
+    public class AmenitieController : BaseWebController
+    {
+        //
+        // GET: /Amenitie/
+
+        public ActionResult Index()
+        {
+            BaseClient client = new BaseClient(baseApiUrl, "amenitie", "getAmenities");
+            List<Amenitie> amenities = new List<Amenitie>();
+            amenities= client.Get<List<Amenitie>>();
+            return View(amenities);
+        }
+
+        //
+        // GET: /Amenitie/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Amenitie/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Amenitie/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Amenitie/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Amenitie/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Amenitie/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Amenitie/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ITZRealStateWeb/Controllers/BaseWebController.cs
@@ -0,0 +1,210 @@
+﻿using ITZRealStateWeb.Helpers;
+using ITZRealStateWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace ITZRealStateWeb.Controllers
+{
+    public class BaseWebController : Controller
+    {
+
+        public BaseWebController()
+        {
+            //if (WebSecurity.Initialized == false)
+            //{
+            //    SimpleMembershipInitializer();
+            //}
+        }
+
+        public bool SimpleMembershipInitializer()
+        {
+            Database.SetInitializer<UsersContext>(null);
+            try
+            {
+                using (var context = new UsersContext())
+                {
+                    if (!context.Database.Exists())
+                    {
+                        ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                    }
+                }
+                WebSecurity.InitializeDatabaseConnection("BusinessLMSContext", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+            }
+        }
+
+        //        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        //        {
+        //            Cookies = new Helpers.Cookies(filterContext.HttpContext);
+        //            base.OnActionExecuting(filterContext);
+        //        }
+
+        //        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        //        {
+        //            String actionName = filterContext.ActionDescriptor.ActionName;
+        //            String controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+        //            try
+        //            {
+        //                List<string> phoneAgents = new List<string>() { "ipod", "iphone", "android 2", "blackberry", "opera mobi", "windows phone" };
+        //                MobileRedirectUtility mru = new MobileRedirectUtility(phoneAgents);
+        //                if (mru.IsPhone())
+        //                {
+        //                    filterContext.Result = Redirect(ConfigurationManager.AppSettings["MobileUrl"]);
+        //                }
+        //                else
+        //                {
+        //                    if ((WebSecurity.IsAuthenticated) && (actionName != "LogOff"))
+        //                    {
+        //                        if (ibo != null)
+        //                        {
+        //                            ViewBag.IBOName = String.Concat(ibo.firstName, " ", ibo.lastName);
+        //                            ViewBag.IBONum = ibo.IBONum;
+        //                            ViewBag.IBOPicture = ibo.picture != String.Empty ? ibo.picture : Url.Content("~/Images/noProfilePicture.png");
+        //                            ViewBag.MenuItems = menuItems;
+        //                            ViewBag.AlertItems = listAlerts;
+        //                            ViewBag.FollowupsCount = Followups.Count;
+        //                        }
+        //                        else
+        //                        {
+        //                            if ((actionName != "AddIBO") && (controllerName != "IBO"))
+        //                            {
+        //                                filterContext.Result = RedirectToAction("AddIBO", "IBO");
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //            catch { }
+        //            ViewBag.actionName = actionName;
+        //            ViewBag.controllerName = controllerName;
+        //            base.OnActionExecuted(filterContext);
+        //        }
+
+        //        internal Helpers.Cookies Cookies;
+
+        //        #region General Properties
+
+        //        #region Not Cookie Saved
+
+        //        public List<Alert> listAlerts
+        //        {
+        //            get
+        //            {
+        //                BaseClient client = new BaseClient(baseApiUrl, "Alerts", "GetAlertsIBO");
+        //                List<Alert> _alerts = client.Get<List<Alert>>(ibo.IBONum);
+        //                return _alerts;
+        //            }
+        //        }
+
+        //        public List<ContactFollowup> Followups
+        //        {
+        //            get
+        //            {
+        //                BaseClient client = new BaseClient(baseApiUrl, "ContactFollowup", "GetIBOFollowup");
+        //                List<ContactFollowup> _followups = client.Get<List<ContactFollowup>>(ibo.IBONum);
+        //                return _followups;
+        //            }
+        //        }
+
+        //        #endregion
+
+        //        #region Cookie Saved
+
+        //        public string FacebookId
+        //        {
+        //            get
+        //            {
+        //                return Cookies.fidCookie.GetFacebookID();
+        //            }
+        //            set
+        //            {
+        //                if (value != null) Cookies.fidCookie.SetFacebookID(value); else Cookies.fidCookie.Nullify();
+        //            }
+        //        }
+
+        //        public string AccessToken
+        //        {
+        //            get
+        //            {
+        //                return Cookies.atCookie.GetAccessToken();
+        //            }
+        //            set
+        //            {
+        //                if (value != null) Cookies.atCookie.SetAccessToken(value); else Cookies.atCookie.Nullify();
+        //            }
+        //        }
+
+        //        public IBO ibo
+        //        {
+        //            get
+        //            {
+        //                IBO _ibo = Cookies.iboCookie.GetIBO();
+        //                if (_ibo == null)
+        //                {
+        //                    BaseClient client = new BaseClient(baseApiUrl, "IBO", "GetIBOByUId");
+        //                    _ibo = client.Get<IBO>(WebSecurity.CurrentUserId.ToString());
+        //                    Cookies.iboCookie.SetIBO(_ibo);
+        //                }
+        //                return _ibo;
+        //            }
+        //        }
+
+        //        public List<Step> menuItems
+        //        {
+        //            get
+        //            {
+        //                List<Step> _menuItems = Cookies.menuItemsCooke.GetMenuItems();
+        //                if (_menuItems == null)
+        //                {
+        //                    BaseClient client = new BaseClient(baseApiUrl, "Step", "GetSteps");
+        //                    _menuItems = client.Get<List<Step>>();
+        //                    Cookies.menuItemsCooke.SetMenuItems(_menuItems);
+        //                }
+        //                return _menuItems;
+        //            }
+        //        }
+
+        //        #endregion
+
+        //        #region From Configuration
+
+        public string baseApiUrl
+        {
+            get { return ConfigurationManager.AppSettings["ApiUrl"]; }
+        }
+
+        //        public long eventbriteOrginizerId
+        //        {
+        //            get
+        //            {
+        //                string oId = ConfigurationManager.AppSettings["EventbriteOrginizerId"];
+        //                return long.Parse(oId);
+        //            }
+        //        }
+
+        //        public string eventbriteUserKey
+        //        {
+        //            get { return ConfigurationManager.AppSettings["EventbriteUserKey"]; }
+        //        }
+
+        //        public string eventbriteApiKey
+        //        {
+        //            get { return ConfigurationManager.AppSettings["EventbriteApiKey"]; }
+        //        }
+
+        //        #endregion
+
+        //        #endregion
+
+        //    }
+    }
+}
