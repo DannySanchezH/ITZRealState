9386de4e6862cd0362c8a885e58854ab66491892
Daniela Sanchez <dannysarahy@hotmail.com>
2013-05-28 22:41:56 -0500

    Facebook Log in Finish

--- ITZRealStateWeb/Controllers/AccountController.cs
+++ ITZRealStateWeb/Controllers/AccountController.cs
@@ -81,7 +81,7 @@ namespace ITZRealStateWeb.Controllers
                 {
                     WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
                     WebSecurity.Login(model.UserName, model.Password);
-                    return RedirectToAction("Index", "Dashboard",model);
+                    return RedirectToAction("Index", "Dashboard");
                 }
                 catch (MembershipCreateUserException e)
                 {
@@ -303,25 +303,8 @@ namespace ITZRealStateWeb.Controllers
         [ChildActionOnly]
         public ActionResult ExternalLoginsList(string returnUrl)
         {
-            List<AuthenticationClientData> clientData = new List<AuthenticationClientData>();
-            if (WebSecurity.IsAuthenticated)
-            {
-                List<string> accounts = (from account in OAuthWebSecurity.GetAccountsFromUserName(User.Identity.Name) select account.Provider).ToList();
-                ICollection<AuthenticationClientData> allClients = OAuthWebSecurity.RegisteredClientData;
-                foreach (AuthenticationClientData client in allClients)
-                {
-                    if (accounts.Contains(client.AuthenticationClient.ProviderName) == false)
-                    {
-                        clientData.Add(client);
-                    }
-                }
-            }
-            else
-            {
-                clientData = OAuthWebSecurity.RegisteredClientData.ToList();
-            }
             ViewBag.ReturnUrl = returnUrl;
-            return PartialView("_ExternalLoginsListPartial", clientData);
+            return PartialView("_ExternalLoginsListPartial", OAuthWebSecurity.RegisteredClientData);
         }
 
         [ChildActionOnly]
